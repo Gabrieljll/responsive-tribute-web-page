c28b719ad2e79f07255abbeb494a6e300846f527
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2022-01-31 01:02:23 -0300

    Adding buttons to read the timeline

--- js/index.js
+++ js/index.js
@@ -1,12 +1,25 @@
 function slider() {
-    var counter = 1;
-    setInterval(function() {
-        document.getElementById('radio' + counter).checked = true;
-        counter++;
-        if (counter > 4) {
-            counter = 1;
-        }
-    }, 3000);
+  var counter = 1;
+  setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+      counter = 1;
+    }
+  }, 3000);
+}
+
+function changeInformationSection(btnSelected) {
+  let idBtnSelected = btnSelected.id
+  let divBtn = btnSelected.parentNode
+  let divInformation = divBtn.parentNode
+  divBtn.style.display = 'none'
+  divInformation.style.display = 'none'
+    //Slice '4' chars beacase the first four chacaracters are 'btn-' and the rest is the id 's div we need
+  let divToShow = document.getElementById(idBtnSelected.slice(4))
+  divToShow.style.display = 'inline-grid'
+  divToShow.style.justifyContent = 'center'
+  divToShow.firstChild.style.display = 'flex !important'
 }
 
 window.onload = slider();
