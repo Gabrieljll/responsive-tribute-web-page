65a82d23aa35f22343a99d45607168a3ae65b4dc
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2022-10-08 22:30:14 -0300

    Color changes

--- js/index.js
+++ js/index.js
@@ -13,13 +13,24 @@ function changeInformationSection(btnSelected) {
   let idBtnSelected = btnSelected.id
   let divBtn = btnSelected.parentNode
   let divInformation = divBtn.parentNode
+  aplicarFade(divInformation, "0")
   divBtn.style.display = 'none'
   divInformation.style.display = 'none'
-    //Slice '4' chars beacase the first four chacaracters are 'btn-' and the rest is the id 's div we need
+
+
+  //Slice '4' chars beacase the first four chacaracters are 'btn-' and the rest is the id 's div we need
   let divToShow = document.getElementById(idBtnSelected.slice(4))
   divToShow.style.display = 'inline-grid'
+  aplicarFade(divToShow, "1")
   divToShow.style.justifyContent = 'center'
-  divToShow.firstChild.style.display = 'flex !important'
+  divToShow.firstChild.style = 'display:flex !important'
+}
+
+function aplicarFade(divParent, fade) {
+  for (let i = 0; i < divParent.childNodes.length; i++) {
+    if (divParent.childNodes[i].nodeName === 'p')
+      divParent.childNodes[i].style.opacity = fade
+  }
 }
 
 window.onload = slider();
